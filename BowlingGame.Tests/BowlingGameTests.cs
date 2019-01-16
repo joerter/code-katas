bb81bbe2aaaa081096ba664511c6301628bbcc82
John Oerter <john.oerter@fcsamerica.com>
2019-01-16 08:23:23 -0600

    Complete Kata

--- BowlingGame.Tests/BowlingGameTests.cs
+++ BowlingGame.Tests/BowlingGameTests.cs
@@ -1,14 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace BowlingGame.Tests
 {
     [TestFixture]
     public class BowlingGameTests
     {
+        private Game _game;
+
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void GivenScoreWhenGutterGameThenScoreIs0()
+        {
+            RollMany(20, 0);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(0, score);
+        }
+
+        [Test]
+        public void GivenScoreWhenAllOnesAreRolledThenScoreIs20()
+        {
+            RollMany(20, 1);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(20, score);
+        }
+
+        [Test]
+        public void GivenScoreWhenOneSpaceThenCorrectScore()
+        {
+            RollSpare();
+            _game.Roll(3);
+            RollMany(17, 0);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(16, score);
+        }
+
+        [Test]
+        public void GivenScoreWhenOneStrikeThenCorrectScore()
+        {
+            RollStrike();
+            _game.Roll(3);
+            _game.Roll(3);
+            RollMany(17, 0);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(22, score);
+        }
+
+        [Test]
+        public void GivenScoreWhenPerfectGameThen300()
+        {
+            RollMany(12, 10);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(300, score);
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
     }
 }
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -3,19 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace BowlingGame
 {
     public class Game
     {
+        private List<int> _rolls = new List<int>();
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            _rolls.Add(pins);
         }
 
         public int Score()
         {
-            throw new NotImplementedException();
+            var score = 0;
+            var rollIndex = 0;
+
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (RollIsStrike(rollIndex))
+                {
+                    score += 10 + StrikeBonus(rollIndex);
+                    rollIndex++;
+                    continue;
+                }
+
+                var frameScore = _rolls[rollIndex] + _rolls[rollIndex + 1];
+
+                if (FrameIsSpare(frameScore))
+                {
+                    score += 10 + SpareBonus(rollIndex);
+                }
+                else
+                {
+                    score += frameScore;
+                }
+
+                rollIndex += 2;
+            }
+
+            return score;
+        }
+
+        private int StrikeBonus(int rollIndex)
+        {
+            return _rolls[rollIndex + 1] + _rolls[rollIndex + 2];
+        }
+
+        private bool RollIsStrike(int rollIndex)
+        {
+            return _rolls[rollIndex] == 10;
+        }
+
+        private int SpareBonus(int rollIndex)
+        {
+            return _rolls[rollIndex + 2];
+        }
+
+        private static bool FrameIsSpare(int frameScore)
+        {
+            return frameScore == 10;
         }
     }
 }
