fc231a19ac60c321fc25384e7e643732c9b95fd4
John Oerter <john.oerter@fcsamerica.com>
2019-01-16 13:18:06 -0600

    First coding dojo session

--- BowlingGame.Tests/BowlingGameTests.cs
+++ BowlingGame.Tests/BowlingGameTests.cs
@@ -5,5 +5,66 @@ namespace BowlingGame.Tests
     [TestFixture]
     public class BowlingGameTests
     {
+        private Game _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Game();
+        }
+
+        public void SetupRolls(int pinsPerRoll, int rolls)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _game.Roll(pinsPerRoll);
+            }
+        }
+
+        [Test]
+        public void GivenScore_WhenAllGutterBalls_ThenScoreShouldBeZero()
+        {
+            SetupRolls(0, 20);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(0, score);
+        }
+
+        [Test]
+        public void GivenScore_WhenRollOfOne_ThenScoreShouldBeOne()
+        {
+            SetupRolls(1, 1);
+            SetupRolls(0, 19);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(1, score);
+        }
+
+        [Test]
+        public void GivenScore_WhenRollOneSpareAndThenGuttersOnlyInGame_ThenScoreShouldBe10()
+        {
+            SetupRolls(1, 1);
+            SetupRolls(9, 1);
+            SetupRolls(0, 18);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(10, score);
+        }
+
+        [Test]
+        public void GivenScore_WhenRollOneSpareAnd1PinInNextTurnInGame_ThenScoreShouldBe12()
+        {
+            SetupRolls(1, 1);
+            SetupRolls(9, 1);
+            SetupRolls(1, 1);
+            SetupRolls(0, 17);
+
+            var score = _game.Score();
+
+            Assert.AreEqual(12, score);
+        }
     }
 }
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private readonly List<int> _rolls = new List<int>();
+        private bool _isPreviousFrameSpare = false;
+
+        public void Roll(int pins)
+        {
+            _rolls.Add(pins);
+
+            if (_rolls.Count % 2 == 0)
+            {
+                if (_rolls[_rolls.Count - 2] + pins == 10)
+                {
+                    _isPreviousFrameSpare = true;
+                }
+            }
+            else
+            {
+                if (_isPreviousFrameSpare)
+                {
+                }
+            }
+        }
+
+        public int Score()
+        {
+            return _rolls.Sum();
+        }
+    }
+}
