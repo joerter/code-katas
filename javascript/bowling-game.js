9707cd59c9b66a18d553b42367a251d7ccf62e01
Kile Donley <kiledonley@gmail.com>
2019-08-24 15:40:06 -0500

    Added javascript version

---
+++ javascript/bowling-game.js
@@ -0,0 +1,55 @@
+class Game {
+    constructor() {
+        this.rolls = [];
+        this.rollIndex = 0;
+    }
+
+    roll(pins) {
+        this.rolls[this.rollIndex++] = pins;
+    }
+    
+
+    get score() {
+        console.log(this.rolls);
+        let gameScore = 0;
+        let rollIndex = 0;
+
+        for(let frame = 0; frame < 10; frame++){
+            let frameScore = this.rolls[rollIndex] + this.rolls[rollIndex+1]
+
+            if (this.isStrike(this.rolls[rollIndex])) {
+                gameScore += this.strikeBonus(rollIndex);
+                rollIndex += 1;
+                continue;
+            }
+
+            if (this.isSpare(frameScore)) {
+                gameScore += this.spareBonus(rollIndex);
+            } else {
+                gameScore += frameScore;
+            }
+
+            rollIndex += 2
+        }
+
+        return gameScore;
+    }
+
+    isStrike(pins) {
+        return pins === 10;
+    }
+
+    isSpare(frameScore) {
+        return frameScore === 10;
+    }
+
+    spareBonus(index) {
+        return 10 + this.rolls[index + 2];
+    }
+
+    strikeBonus(index) {
+        return 10 + this.rolls[index + 1] + this.rolls[index + 2];
+    }
+}
+
+module.exports = Game;
--- javascript/bowling-game.test.js
+++ javascript/bowling-game.test.js
@@ -1,1 +1,49 @@
 const Game = require('./bowling-game');
+
+let game;
+
+beforeEach(() => {
+    game = new Game();
+})
+
+test('all gutter balls', () => {
+    rollMany(20, 0);
+
+    expect(game.score).toEqual(0);
+})
+
+test('all Ones', () => {
+    rollMany(20, 1);
+
+    expect(game.score).toEqual(20);
+})
+
+test('one spare', () => {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(17, 0);
+
+    expect(game.score).toEqual(16);
+})
+
+test('one stike', () => {
+    game.roll(10);
+    game.roll(1);
+    game.roll(1);
+    rollMany(17, 0);
+
+    expect(game.score).toEqual(14);
+})
+
+test('perfect game', () => {
+    rollMany(12, 10);
+
+    expect(game.score).toEqual(300);
+})
+
+const rollMany = (rolls, pins) => {
+    for (let roll = 0; roll < rolls; roll++) {
+        game.roll(pins);
+    }
+}
