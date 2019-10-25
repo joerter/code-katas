92b1b01a0cfe8c3ef07ceaccc7099fb9641c7902
John Oerter <john.oerter@fcsamerica.com>
2019-10-25 07:56:05 -0500

    Pre monads

--- bowling-game/c-sharp/BowlingGame.Tests/BowlingGameTests.cs
+++ bowling-game/c-sharp/BowlingGame.Tests/BowlingGameTests.cs
@@ -5,6 +5,66 @@ namespace BowlingGame.Tests
     [TestFixture]
     public class BowlingGameTests
     {
+        private Game _game;
 
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+        }
+
+        [Test]
+        public void GivenGame_WhenAllGutterBalls_ThenScoreReturns0()
+        {
+            RollMany(0, 20);
+
+            Assert.AreEqual(0, _game.Score());
+        }
+
+        [Test]
+        public void GivenGame_WhenAllOnes_ThenScoreReturns20()
+        {
+            RollMany(1, 20);
+
+            Assert.AreEqual(20, _game.Score());
+        }
+
+        [Test]
+        public void GivenGame_WhenRollOneSpare_ThenScoreReturnsCorrectly()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+            RollMany(0, 17);
+
+            Assert.AreEqual(16, _game.Score());
+        }
+
+        [Test]
+        public void GivenGame_WhenRollOneStrike_ThenScoreReturnsCorrectly()
+        {
+            _game.Roll(10);
+            _game.Roll(1);
+            _game.Roll(1);
+            RollMany(0, 17);
+
+            Assert.AreEqual(14, _game.Score());
+        }
+
+        [Test]
+        public void GivenGame_WhenPerfectGame_ThenScoreReturns300()
+        {
+            RollMany(10, 22);
+
+            Assert.AreEqual(300, _game.Score());
+        }
+
+        private void RollMany(int pins, int rolls)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
     }
 }
---
+++ bowling-game/c-sharp/BowlingGame/Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private const int AllPins = 10;
+        public List<int> Rolls = new List<int>();
+
+        private readonly Dictionary<int, Func<int, int>> _spareBonus;
+        private readonly Dictionary<int, Func<int, int>> _strikeBonus;
+
+        public Game()
+        {
+            _spareBonus = new Dictionary<int, Func<int, int>>
+            {
+                {10, rollIndex => 10 + Rolls[rollIndex + 2]}
+            };
+
+            _strikeBonus = new Dictionary<int, Func<int, int>>
+            {
+                {10, rollIndex => 10 + Rolls[rollIndex + 1] + Rolls[rollIndex + 2]}
+            };
+        }
+
+        public void Roll(int pins)
+        {
+            Rolls.Add(pins);
+        }
+
+
+        public int Score()
+        {
+            var frames = Enumerable.Range(0, 10);
+
+            var startingGameState = new GameState
+            {
+                Score = 0,
+                RollIndex = 0
+            };
+            return frames.Aggregate(startingGameState, (state, frameIndex) =>
+            {
+                var pins = Rolls[state.RollIndex];
+
+                try
+                {
+                    return new GameState
+                    {
+                        Score = state.Score + _strikeBonus[pins](state.RollIndex),
+                        RollIndex = state.RollIndex + 1
+                    };
+                }
+                catch 
+                {
+                    var frameScore = Rolls[state.RollIndex] + Rolls[state.RollIndex + 1];
+                    try
+                    {
+                        return new GameState
+                        {
+                            Score = state.Score + _spareBonus[frameScore](state.RollIndex),
+                            RollIndex = state.RollIndex + 2
+                        };
+                    }
+                    catch
+                    {
+                        return new GameState
+                        {
+                            Score = state.Score + frameScore,
+                            RollIndex = state.RollIndex + 2
+                        };
+                    }
+                }
+
+            }).Score;
+        }
+
+        private int StrikeBonus(int rollIndex)
+        {
+            return 10 + Rolls[rollIndex + 1] + Rolls[rollIndex + 2];
+        }
+
+        private int SpareBonus(int rollIndex)
+        {
+            return 10 + Rolls[rollIndex + 2];
+        }
+    }
+
+    public class GameState
+    {
+        public int Score { get; set; }
+        public int RollIndex { get; set; }
+    }
+}
